74bcd99625bd8eaa67fcdf330e08a15809f6b73b
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-02-14 09:48:50 +0900

    refactor: 시간복잡도 개선

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -5,11 +5,15 @@ def solution(N, stages):
     
     for stage in stages:
         reach_user[stage] += 1
-        for i in range(1, stage+1):
-            total_user[i] += 1
+        # for i in range(1, stage+1):
+        #     total_user[i] += 1
+        
+    total_user[N + 1] = reach_user[N + 1]
+    for i in range(N, 0, -1):
+        total_user[i] = total_user[i + 1] + reach_user[i]
     
-    # print(reach_user)
-    # print(total_user)
+    # print("reach_user" , reach_user)
+    # print("total_user" , total_user)
     
     for i in range(1, N+1):
         if total_user[i] != 0:
