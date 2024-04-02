ec4b38309102e5fd5daa03aed75a438b20756061
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-02 14:54:17 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,10 @@
+def solution(my_string, num1, num2):
+    answer = ''
+    answer += my_string[:num1]
+    answer += my_string[num2]
+    answer += my_string[num1+1:num2]
+    answer += my_string[num1]
+    answer += my_string[num2+1:]
+    # answer += my_string[num2:]
+    print(answer)
+    return answer
