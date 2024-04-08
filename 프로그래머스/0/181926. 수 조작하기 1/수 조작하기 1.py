d0274a8161936906a21a5aebcfc28371958445e3
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-08 20:40:38 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.95 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,11 @@
+def solution(n, control):
+    for command in control:
+        if command == "w":
+            n += 1
+        elif command == "s":
+            n -= 1
+        elif command == "d":
+            n += 10
+        else:
+            n -= 10
+    return n
