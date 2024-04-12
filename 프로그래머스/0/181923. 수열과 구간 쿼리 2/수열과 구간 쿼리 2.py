4b0e4bb157de8c8d8202c1863ab61bcbd840ea08
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-12 23:16:13 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 15.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -0,0 +1,13 @@
+def solution(arr, queries):
+    answer = []
+    for s, e, k in queries:
+        tmp = []
+        for i in range(s, e+1):
+            if arr[i] > k:
+                tmp.append(arr[i])
+        
+        if not tmp:
+            answer.append(-1)
+        else:
+            answer.append(min(tmp))
+    return answer
