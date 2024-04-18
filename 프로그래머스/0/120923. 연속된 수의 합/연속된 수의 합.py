218c6516180a2cc971f8818e12e610fb8ae80d49
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-18 17:26:38 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,12 +1,11 @@
 def solution(num, total):
     avg = total // num
     offset = num // 2
+    end = avg + offset
     
     if num % 2 == 1:
-        start = avg - offset
-        end = avg + offset
+        start = avg - offset    
     else:
         start = avg - offset + 1
-        end = avg + offset
 
     return list(range(start, end + 1))
