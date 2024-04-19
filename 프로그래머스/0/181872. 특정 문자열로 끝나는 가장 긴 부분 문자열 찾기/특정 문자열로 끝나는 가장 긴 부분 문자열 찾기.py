142078d3098cecc327234c83c08f6eaead3f7846
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-19 10:43:30 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -2,16 +2,14 @@ def solution(myString, pat):
     max_length = 0
     longest_substring = ""
 
-    # myString의 각 인덱스를 시작점으로 하여 검사
-    for start in range(len(myString)):
-        # 현재 시작점부터 myString의 끝까지 슬라이싱
-        for end in range(start + len(pat), len(myString)+1):
-            substring = myString[start:end]
-            # 부분 문자열이 pat으로 끝나는지 확인
-            if substring.endswith(pat):
-                # 가장 긴 부분 문자열을 갱신
-                if len(substring) > max_length:
-                    max_length = len(substring)
-                    longest_substring = substring
+    # 현재 시작점부터 myString의 끝까지 슬라이싱
+    for end in range(len(myString)+1):
+        substring = myString[:end]
+        # 부분 문자열이 pat으로 끝나는지 확인
+        if substring.endswith(pat):
+            # 가장 긴 부분 문자열을 갱신
+            if len(substring) > max_length:
+                max_length = len(substring)
+                longest_substring = substring
 
     return longest_substring
