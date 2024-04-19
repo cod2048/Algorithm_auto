3e51fe31424205054660f645829d613f46642301
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-19 11:45:44 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 3.54 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,4 +1,4 @@
 def solution(myString):
-    split_strings = [s for s in myString.split("x") if s]
-    sorted_array = sorted(split_strings)
-    return sorted_array
+    answer = [s for s in myString.split("x") if s]
+    answer.sort()
+    return answer
