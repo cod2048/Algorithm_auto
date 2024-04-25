f3d18c83c916447562541f4cbbf538dabc458266
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-25 11:02:24 +0900

    Time: 44 ms (25.77%), Space: 16.6 MB (28.4%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.py
+++ 0837-most-common-word/0837-most-common-word.py
@@ -1,8 +1,18 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         paragraph = paragraph.lower()
-        word_list = [word for word in re.sub(r"[^\w]", " ", paragraph).split() if word]
         word_dict = dict()
+        new_paragraph = ""
+
+        for letter in paragraph:
+            if letter == " ":
+                new_paragraph += letter
+            if letter.isalnum() == False:
+                new_paragraph += " "
+            else:
+                new_paragraph += letter
+        
+        word_list = [word for word in new_paragraph.split() if word]
 
         for word in word_list:
             if word in banned:
