b914b57e82230a7bf6d0b5e7cef8cb9f019170a2
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-04-25 10:39:44 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph = paragraph.replace(",", "")
+        paragraph = paragraph.replace(".", "")
+        paragraph = paragraph.lower()
+        word_list = [word for word in paragraph.split(" ") if word]
+        word_dict = dict()
+
+        for word in word_list:
+            if word in banned:
+                continue
+
+            if word in word_dict:
+                word_dict[word] += 1
+
+            else:
+                word_dict[word] = 1
+                
+        most_word = sorted(word_dict.items(), key = lambda x : x[1], reverse = True)
+
+        return most_word[0][0]
