cd69ab99890b7bae71a2ba5c75424a56cd472fa1
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-05-05 18:56:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,14 +1,3 @@
 def solution(n, m):
-    min_num = min(n, m)
-    max_num = max(n, m)
-    gcd = 0
-    
-    for num in range(1, min_num + 1):
-        if min_num % num == 0 and max_num % num == 0:
-            gcd = num
-    
-    lcm = n*m // gcd
-    
-    answer = [gcd, lcm]
-    
+    answer = []
     return answer
