fc329e089a3e0af7b519684c21f7f83b2630e147
Junik Park <122762083+cod2048@users.noreply.github.com>
2024-05-17 09:48:52 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,11 +1,15 @@
 def solution(n):
     answer = 0
     formation_3 = ""
+    i = 1
     
     while n > 0:
         formation_3 += str(n % 3)
         n = n // 3
     
-    answer = int(formation_3, 3)
+    for j in range(len(formation_3)-1, -1 ,-1):
+        answer += i * int(formation_3[j])
+        i *= 3
+        
     
     return answer
