14477cc6a523e87340de6d05d23eef523e29db97
Junik <122762083+cod2048@users.noreply.github.com>
2024-07-18 00:55:59 +0900

    [Bronze V] Title: A-B, Time: 132 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] numbers = scanner.readLine().split(" ");
+        int[] array = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(array[0] - array[1]);
+    }
+}
