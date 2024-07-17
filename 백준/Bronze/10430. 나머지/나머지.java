59f5e042cd6ac54aa931ba9b15257c4e9bce14bd
Junik <122762083+cod2048@users.noreply.github.com>
2024-07-18 01:13:00 +0900

    [Bronze V] Title: 나머지, Time: 132 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] numbers = bufferedReader.readLine().split(" ");
+        int[] array = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println((array[0] + array[1]) % array[2]);
+        System.out.println(((array[0] % array[2]) + (array[1] % array[2])) % array[2]);
+        System.out.println((array[0] * array[1]) % array[2]);
+        System.out.println(((array[0] % array[2]) * (array[1] % array[2])) % array[2]);
+    }
+}
